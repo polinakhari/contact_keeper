3f86078c126a1c77914f4cac082c9585ab26125a
polinakhari <polina14.05@yandex.ru>
2020-04-16 21:53:14 +0300

    first

---
+++ routes/contacts.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const router  = express.Router();
+
+//@route POST api/contacts
+//@desc  Add new contact
+//@access Private
+ 
+router.post('/', (req,res) => {
+    res.send('Add contact'); 
+ });
+
+//@route PUT api/contacts/:id
+//@desc  Update contacts
+//@access Private
+ 
+router.put('/:id', (req,res) => {
+    res.send('Update contacts'); 
+ });
+
+//@route GET api/contacts
+//@desc  Get all users contacts
+//@access Private
+ 
+router.get('/', (req,res) => {
+   res.send('Get all contacts'); 
+});
+
+//@route DELETE api/contacts/:id
+//@desc  Update contacts
+//@access Private
+ 
+router.delete('/:id', (req,res) => {
+    res.send('Delete contacts'); 
+ });
+
+
+module.exports = router;
